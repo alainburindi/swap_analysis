af52da1922d4dad28e14c8ad5f6d03baaa10f7d7
alainburindi <alainburindi62@gmail.com>
2020-10-04 07:48:32 +0200

    allow user to login

--- swap_analysis/urls.py
+++ swap_analysis/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from swap_analysis.apps.authentication.views import CustomAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('api-token-auth/', CustomAuthToken.as_view())
 ]
